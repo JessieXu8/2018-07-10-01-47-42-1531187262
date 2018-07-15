76137e9a7d7524e29c09746d98fd533d212f7e32
jessie <jessie.xu@oocl.com>
2018-07-16 00:59:22 +0800

    completed the collection test

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -4,6 +4,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Reduce {
 
@@ -40,22 +41,48 @@ public class Reduce {
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        int firstEvent = arrayList.stream().filter(x->x%2==0).findFirst().get();
+        return arrayList.indexOf(firstEvent);
     }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    public boolean isEqual(List<Integer> objectList) {
+//        throw new NotImplementedException();
+        Boolean isEqual = false;
+
+        if (objectList.size() == arrayList.size()) {
+
+            isEqual = IntStream.range(0, arrayList.size())
+
+                    .allMatch(index -> arrayList.get(index) == objectList.get(index));
+
+        }
+
+        return isEqual;
     }
+
 
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        int index = arrayList.size() / 2;
+        if (arrayList.size() % 2 == 0) {
+
+            return (singleLink.getNode(index) + singleLink.getNode(index + 1)) / 2.0;
+
+        } else {
+
+            return singleLink.getNode(index + 1);
+
+        }
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return arrayList.stream().filter(num -> num % 2 != 0).reduce((first, second) -> second).get();
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return arrayList.lastIndexOf(getLastOdd());
     }
 }
